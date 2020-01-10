bfca5a64fd40a686e1e8969bdb16ea9bdf7ecaa9
Isabel Uriarte <uriarte.isabel@gmail.com>
2020-01-09 16:27:01 -0800

    wip

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express")
 const mySQL = require("mysql")
+const path = require("path")
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+// app.post()
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
